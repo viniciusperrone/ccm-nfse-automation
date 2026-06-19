c2d2846e93666fb4c63d7d83ee847d66d4abf15d
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-06-19 16:20:10 -0300

    fix: initialize base scraper attributes correctly

--- scrapers/belo_horizonte.py
+++ scrapers/belo_horizonte.py
@@ -3,15 +3,13 @@ from base.config import Config
 
 
 class BeloHorizonteScraper(BaseScraper):
-
-    def __init__(self):
-        self.ccm_url = Config.get_ccm("BELO_HORIZONTE")
+    CITY = "BELO_HORIZONTE"
 
     async def scrape(self):
-        await self.page.goto(self.ccm_url)
+        await self.page.goto(Config.get_ccm(self.CITY))
 
         await self.page.locator(
-            "#corpo\\:formulario\\:identificador"
+            '[id="corpo:formulario:identificador"]'
         ).fill("28203865000174")
 
         await self.page.pause()
